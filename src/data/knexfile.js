a38159b376bef8132863c837098c777a5a0d9ac3
Yuval Ginosar <yuvalginosar@gmail.com>
2022-05-14 18:57:48 +0300

    corrections fo deploy

--- src/data/knexfile.js
+++ src/data/knexfile.js
@@ -1,16 +1,18 @@
 import path from 'path';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import "dotenv/config";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const pathToMigrations = path.resolve(__dirname, '../migrations');
 
 const knexConfig = {
   client: 'mysql',
-  connection: {
-    database: 'petsapp',
-    user: 'root',
-    password: '321ginosar',
-  },
+  connection: process.env.DB_CONNECTION,
+  // {
+  //   database: 'petsapp',
+  //   user: 'root',
+  //   password: '321ginosar',
+  // },
   pool: {
     min: 2,
     max: 10,
