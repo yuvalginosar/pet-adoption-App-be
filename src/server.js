96efff66fad4819081d519da38415f39d73d5fbf
Yuval Ginosar <yuvalginosar@gmail.com>
2022-05-15 19:33:21 +0300

    herolu cors

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ app.use(
   express.static(process.env.UPLOAD_FOLDER)
 );
 app.use(express.json());
-app.use(cors({ origin: ["http://localhost:3000"], credentials: true }));
+app.use(cors({ origin: ["http://localhost:3000", 'https://fe-pet-adoption.herokuapp.com'], credentials: true }));
 app.use(cookieParser());
 
 app.use("/pet", petsRoutes);
